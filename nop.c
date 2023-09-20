686cea92a7afea40f5e5c06e7a09d4aacefa95a1
chiro999 <r.o.g.u.e@outlook.com>
2023-09-20 08:45:01 -0700

    nop

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * _nop - Do nothing.
+ * @head: stack head
+ * @line_counter: line number
+ * Return: no return
+ */
+void _nop(stack_t **head, unsigned int line_counter)
+{
+    (void) line_counter;
+    (void) head;
+}
