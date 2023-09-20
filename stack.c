5e64fae4219ea4fee1a71b005f761cfc990e62cc
chiro999 <r.o.g.u.e@outlook.com>
2023-09-20 09:06:31 -0700

    stack

---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * _stack - prints the top
+ * @head: stack head
+ * @line_counter: line_number
+ * Return: no return
+ */
+void _stack(stack_t **head, unsigned int line_counter)
+{
+    (void) head;
+    (void) line_counter;
+    program.change_ds = 0; // Changed from bus.lifi to program.change_ds
+}
