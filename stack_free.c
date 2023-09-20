fc6152ebe6f1cfb88ed0694b80d9353bdb6bffe0
chiro999 <r.o.g.u.e@outlook.com>
2023-09-20 09:16:27 -0700

    free

---
+++ stack_free.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+/**
+ * stack_free - frees a doubly linked list
+ * @head: head of the stack
+ */
+void stack_free(stack_t *head)
+{
+    stack_t *tmp; // Changed from 'aux'
+
+    tmp = head;
+    while (head)
+    {
+        tmp = head->next;
+        free(head);
+        head = tmp;
+    }
+}
