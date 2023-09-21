0cb1d264804eee2dd5feb3484390d4b19e2bb8b7
ezehchekwubee <mrposhboo@gmail.com>
2023-09-21 13:06:50 -0700

    correct

--- div.c
+++ div.c
@@ -1,39 +1,6 @@
-#include "monty.h"
-/**
- * div_stack2 - divides the top two elements of the stack.
- * @head: stack head
- * @line_counter: line number
- * Return: no return
- */
-void div_stack2(stack_t **head, unsigned int line_counter)
-{
-    int len = 0;
-    int tmp;
-
-    /* Check if there are at least two elements in the stack */
-    if (*head == NULL || (*head)->next == NULL)
-    {
-        fprintf(stderr, "L%d: can't div, stack too short\n", line_counter);
-        fclose(program.file);
-        free(program.line_content);
-        stack_free(*head);
-        exit(EXIT_FAILURE);
-    }
-
-    /* Create a new node to hold the result of the division */
-    stack_t *new_node = malloc(sizeof(stack_t));
-    if (new_node == NULL)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        fclose(program.file);
-        free(program.line_content);
-        stack_free(*head);
-        exit(EXIT_FAILURE);
-    }
-
+void div_stack2(stack_t **head, unsigned int line_counter) {
     /* Check for division by zero */
-    if ((*head)->n == 0)
-    {
+    if ((*head)->n == 0) {
         fprintf(stderr, "L%d: division by zero\n", line_counter);
         fclose(program.file);
         free(program.line_content);
@@ -42,7 +9,17 @@ void div_stack2(stack_t **head, unsigned int line_counter)
     }
 
     /* Calculate the result of the division and store it in 'tmp' */
-    tmp = (*head)->next->n / (*head)->n;
+    int tmp = (*head)->next->n / (*head)->n;
+
+    /* Create a new node */
+    stack_t *new_node = malloc(sizeof(stack_t));
+    if (new_node == NULL) {
+        fprintf(stderr, "Error: malloc failed\n");
+        fclose(program.file);
+        free(program.line_content);
+        stack_free(*head);
+        exit(EXIT_FAILURE);
+    }
 
     /* Update the new node's value */
     new_node->n = tmp;
